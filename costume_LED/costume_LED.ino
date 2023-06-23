be66d0ce23ad24d95f1d444c32c8e736068ead59
Alec <alec.zaiane@gmail.com>
2023-06-23 15:17:20 -0600

    fixed time and memory issues

--- costume_LED/costume_LED.ino
+++ costume_LED/costume_LED.ino
@@ -6,17 +6,17 @@ FASTLED_USING_NAMESPACE
 #define DATA_PIN 3
 #define LED_TYPE WS2811
 #define COLOR_ORDER GRB
-#define NUM_LEDS 100
+#define NUM_LEDS 400
 CRGB leds[NUM_LEDS];
 
-#define BRIGHTNESS 50
+#define BRIGHTNESS 99
 #define FPS 50
 
 #define NUM_BUCKETS 5
 #define BUCKET_MAX 100
 const short bucket_length = NUM_LEDS / NUM_BUCKETS;
 short freq_buckets[] = { 10, 25, 50, 75, 100 };
-int prev_millis = 0;
+unsigned long prev_millis = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -25,6 +25,19 @@ void setup() {
   FastLED.setBrightness(BRIGHTNESS);
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
+}
+
+void update_buckets(){
+  for(short i=0; i<NUM_BUCKETS; i++){
+    if(freq_buckets[i] > 1){
+      freq_buckets[i] -= int(random(7));
+      freq_buckets[i] = max(freq_buckets[i], 0);
+    }
+    if(random(11) > 9 || freq_buckets[i] < 1){
+      freq_buckets[i] = max(int(random(BUCKET_MAX)), freq_buckets[i]);
+    }
+  }
+  
 }
 
 uint8_t gHue = 0;  // rotating "base color" used by many of the patterns
@@ -36,21 +49,23 @@ void loop() {
   // put your main code here, to run repeatedly:
   disp_buckets();
   FastLED.show();
-  FastLED.delay(1000 / 10);
+  FastLED.delay(1000 / 50);
   EVERY_N_MILLISECONDS(20) {
     gHue++;
+    update_buckets();
   }  // slowly cycle the "base color" through the rainbow
 }
+
 
 void disp_buckets() {
   // display frequency buckets onto the LED strip
   // directly adapted from simulator code
   short rolling_index = -1; // TODO figure out why this is -1 here and 0 in java
   bool forward = true;
-  for(int i=0; i<NUM_BUCKETS; i++){
+  for(short i=0; i<NUM_BUCKETS; i++){
     //for each strip, draw a solid pattern_1 amount, then a transition pixel, then a solid pattern_2 amount
     float throwaway_amount_pattern_1 = float(bucket_length*freq_buckets[i]) / float(BUCKET_MAX);
-    int full_pattern_1 = int(throwaway_amount_pattern_1);
+    short full_pattern_1 = short(throwaway_amount_pattern_1);
     if(full_pattern_1 == bucket_length) {
       full_pattern_1 -=1;
     }
@@ -58,17 +73,17 @@ void disp_buckets() {
     float full_pattern_2 = bucket_length - full_pattern_1 - 1; // 1 for the transition pixel
     // if the strip is reversed, draw pattern_2 first
     if (!forward) {
-      for(int j=0; j<full_pattern_2; j++){
+      for(short j=0; j<full_pattern_2; j++){
         leds[rolling_index] = pattern_2(rolling_index++);
       }
       leds[rolling_index] = mix_pattern_percent(rolling_index++, transition_pattern_1_amount);
     }
-    for(int j=0; j<full_pattern_1; j++){
+    for(short j=0; j<full_pattern_1; j++){
       leds[rolling_index] = pattern_1(rolling_index++);
     }
     if (forward){
       leds[rolling_index] = mix_pattern_percent(rolling_index++, transition_pattern_1_amount);
-      for(int j=0; j<full_pattern_2; j++) {
+      for(short j=0; j<full_pattern_2; j++) {
         leds[rolling_index] = pattern_2(rolling_index++);
       }
     }
