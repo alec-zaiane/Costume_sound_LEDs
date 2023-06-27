d1932faa645d6bc2c86c2378c72638faa37d2f3a
Alec <alec.zaiane@gmail.com>
2023-06-27 16:34:12 -0600

    refactor

--- costume_LED/costume_LED.ino
+++ costume_LED/costume_LED.ino
@@ -17,27 +17,20 @@ const unsigned char BUCKET_LENGTH = NUM_LEDS / NUM_BUCKETS;  // TODO make this a
 // each bucket is out of 255
 unsigned char freq_buckets[] = { 50, 54, 56, 58, 62 };  //{ 50, 64, 128, 184, 255};//, 25, 60, 80, 9, 55, 11, 23, 84, 92, 99, 50, 44, 80, 10, 34 };
 
+byte p1_mode = 0;
+byte p2_mode = 1;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.println(0);
   delay(3000);
-  FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(0xFFE08C); // its RBG??
   FastLED.setBrightness(BRIGHTNESS);
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void update_buckets() {
-  for (short i = 0; i < NUM_BUCKETS; i++) {
-    if (freq_buckets[i] > 1) {
-      freq_buckets[i] -= int(random(7));
-      freq_buckets[i] = max(freq_buckets[i], 0);
-    }
-    if (random(11) > 9 || freq_buckets[i] < 1) {
-      freq_buckets[i] = max(int(random(255)), freq_buckets[i]);
-    }
-  }
-}
+
 
 uint8_t gHue = 0;  // rotating "base color" used by many of the patterns
 
@@ -72,17 +65,17 @@ void disp_buckets() {
     // if the strip is reversed, draw pattern_2 first
     if (!forward) {
       for (short j = 0; j < full_pattern_2; j++) {
-        leds[rolling_index] = pattern_2(rolling_index++);
+        leds[rolling_index] = pattern(p2_mode, rolling_index++);
       }
       leds[rolling_index] = mix_pattern_percent(rolling_index++, transition_amount);
     }
     for (short j = 0; j < full_pattern_1; j++) {
-      leds[rolling_index] = pattern_1(rolling_index++);
+      leds[rolling_index] = pattern(p1_mode, rolling_index++);
     }
     if (forward) {
       leds[rolling_index] = mix_pattern_percent(rolling_index++, transition_amount);
       for (short j = 0; j < full_pattern_2; j++) {
-        leds[rolling_index] = pattern_2(rolling_index++);
+        leds[rolling_index] = pattern(p2_mode, rolling_index++);
       }
     }
     forward = !forward;
@@ -90,21 +83,31 @@ void disp_buckets() {
 }
 
 CRGB mix_pattern_percent(short index, unsigned char amount) {
-  return blend(pattern_2(index), pattern_1(index), amount);
+  return blend(pattern(p2_mode, index), pattern(p1_mode, index), amount);
 }
 
-CRGB pattern_1(short index) {
-  CRGB out = CRGB::Cyan;
-  if (index % 3 == 0) {
-    out = CRGB::SlateBlue;
+CRGB pattern(byte mode, short index) {
+  // return colour based on mode and index
+  switch (mode){
+    case 0: // cyan/blue 2:1
+      return (index%3 == 0) ? CRGB::Cyan : CRGB::SlateBlue;
+      break;
+    case 1: // red/orange 2:1
+      return (index%3 == 0) ? CRGB::Red : CRGB::Orange;
+      break;
   }
-  return out;
+
 }
 
-CRGB pattern_2(short index) {
-  CRGB out = CRGB::Red;
-  if (index % 3 == 0) {
-    out = CRGB::OrangeRed;
+void update_buckets() {
+  for (short i = 0; i < NUM_BUCKETS; i++) {
+    freq_buckets[i] += 1;
+    // if (freq_buckets[i] > 1) {
+    //   freq_buckets[i] -= int(random(7));
+    //   freq_buckets[i] = max(freq_buckets[i], 0);
+    // }
+    // if (random(11) > 9 || freq_buckets[i] < 1) {
+    //   freq_buckets[i] = max(int(random(255)), freq_buckets[i]);
+    // }
   }
-  return out;
 }
